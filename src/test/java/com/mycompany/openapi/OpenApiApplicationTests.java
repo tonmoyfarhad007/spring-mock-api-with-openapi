ba9f5368c5c580dad9efcb1b774da903c5ff19d9
tonmoyfarhad007 <tonmoy.farhad@gmail.com>
2024-11-11 15:41:52 +0600

    initial commit

---
+++ src/test/java/com/mycompany/openapi/OpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
